fca2e6f23a16f04df829977557dd0f03837d67e9
franciscozv <javierzambranovaldes@gmail.com>
2025-07-09 00:41:50 -0400

    fix error

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,4 +1,4 @@
-import { Button as MuiButton, ButtonProps } from '@mui/material';
+import { Button as MuiButton, type ButtonProps } from '@mui/material';
 
 const Button = (props: ButtonProps) => {
   return <MuiButton {...props} />;
